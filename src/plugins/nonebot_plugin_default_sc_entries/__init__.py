3aef123e847b9ab8b21bbf0eddd12b74d4d5938d
Hongpei Zheng <zhenghongpei@gmail.com>
2022-03-26 13:56:41 +0000

    默认词条更新

---
+++ src/plugins/nonebot_plugin_default_sc_entries/__init__.py
@@ -0,0 +1,23 @@
+from nonebot import get_driver
+from nonebot import get_driver, on_message, Bot, on_startswith
+from nonebot.adapters.onebot.v11 import Bot, Event, Message, GroupMessageEvent
+from .config import default_entries, save_config_to_yaml, current_folder, get_config
+
+import os
+global_config = get_driver().config
+
+
+if not os.path.exists(os.path.join(current_folder, "config.yaml")):
+    save_config_to_yaml(default_entries)
+config: dict = get_config()
+
+get_default_entries = on_startswith('.', priority=30)
+
+
+@get_default_entries.handle()
+async def _get_default_entries(bot: Bot, event: GroupMessageEvent):
+    key = event.get_plaintext().replace('.', '').strip()
+    if key in config:
+        await bot.call_api('send_group_msg', message=config[key], group_id=event.group_id)
+
+
--- src/plugins/nonebot_plugin_help/__init__.py
+++ src/plugins/nonebot_plugin_help/__init__.py
@@ -4,8 +4,14 @@ from .generate_help_image import get_help
 
 
 help_image = on_startswith('.帮助', priority=3, block=True)
+command_image = on_startswith('.指令', priority=3, block=True)
 
 
 @help_image.handle()
 async def _help_image():
     await help_image.finish(Message(MessageSegment.image(await get_help())))
+
+
+@command_image.handle()
+async def _command_image():
+    await command_image.finish(Message(MessageSegment.image(await get_help())))
